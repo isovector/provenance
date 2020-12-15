a2727d24670a5e540aeb9314e489737e0a4e3165
Sandy Maguire <sandy@sandymaguire.me>
2020-12-15 10:32:04 -0800

    track values with TH

--- src/Provenance.hs
+++ src/Provenance.hs
@@ -1,3 +1,25 @@
+{-# LANGUAGE DeriveLift      #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Provenance where
 
 import Language.Haskell.TH
+import Language.Haskell.TH.Syntax (Lift)
+
+
+data Provenance a = Provenance
+  { getProvenance :: Loc
+  , getData :: a
+  }
+  deriving (Eq, Ord, Show, Functor)
+
+deriving instance Lift Loc
+
+
+track :: TExpQ (a -> Provenance a)
+track = do
+  loc <- location
+  [||Provenance loc||]
+
